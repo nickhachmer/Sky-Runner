80fc8d27cf6fdcdf536222a5e02a5495732a5388
Nicholas Hachmer <nicholas.hachmer@gmail.com>
2020-03-31 11:28:08 -0300

    added dev scene and jumping to player sprite

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Player Components
+    public BoxCollider2D boxCollider;
+    public new Rigidbody2D rigidbody;
+
+    // Player Values
+    public int speed = 10;
+    public int jumpForce = 300;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //boxCollider = GetComponent<BoxCollider2D>();
+        //rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            rigidbody.AddForce(new Vector2(0, jumpForce));
+        }
+
+
+    }
+}
