fc48fce51d2dc1f5b7129acb1c7d946b49d94b01
Nicholas Hachmer <nicholas.hachmer@gmail.com>
2020-04-02 10:38:56 -0300

    double jump works

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     #region PlayerMovement Variables
-    // player 
+    // playe
     public BoxCollider2D boxCollider;
     public new Rigidbody2D rigidbody;
     public LayerMask terrainLayer;
@@ -17,32 +17,41 @@ public class PlayerMovement : MonoBehaviour
     public int maxVerticalVelocity;
 
 
-    //private player values
+    // private player values
     private float horizontalAxis = 0;
     private float verticalAxis = 0;
     private bool jumpActive = false;
-    private int jumpCounter = 2;
+    private int jumpCounter = 0;
+    private int maxJumps = 2;
+    private bool jumpPressed = false;
     private bool onGround = false;
-    private bool canAirJump = false;
 
     #endregion
+
+    // used for intialization
+    void Awake() {
+        boxCollider = GetComponent<BoxCollider2D>();
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
 
     // Start is called before the first frame update
     void Start()
     {   
-
-        //boxCollider = GetComponent<BoxCollider2D>();
-        //rigidbody = GetComponent<Rigidbody2D>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
         horizontalAxis = Input.GetAxisRaw("Horizontal");
         verticalAxis = Input.GetAxisRaw("Vertical");
-        // activate jump
-        jumpActive = Input.GetButton("Jump");
+        
+        if (Input.GetButtonDown("Jump") && !jumpPressed) {
+            jumpActive = true;
+            jumpPressed = true;
+        } else if (Input.GetButtonUp("Jump")) {
+            jumpPressed = false;
+        }
     }
 
     void FixedUpdate() {
@@ -50,7 +59,6 @@ public class PlayerMovement : MonoBehaviour
         if (verticalAxis < 0) AccelerateDown();
         if (jumpActive) Jump();
         checkVerticalVeclocity();
-
     }
 
     void OnCollisionEnter2D(Collision2D col) {
@@ -58,16 +66,20 @@ public class PlayerMovement : MonoBehaviour
             Debug.Log("Terrain collision");
             onGround = true;
         }
-        
     }
 
     #region Movement Methods
 
     //to implement double jump will need to check - can't use on collision./
     private void Jump() {
-        Debug.Log("jump");
-        rigidbody.velocity = new Vector2(rigidbody.velocity.x, 0);
-        rigidbody.AddForce(new Vector2(0, jumpForce));
+        if (onGround) jumpCounter = 0; 
+        if (onGround || jumpCounter < maxJumps) {
+            jumpCounter++;
+            onGround = false;
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x, 0);
+            rigidbody.AddForce(new Vector2(0, jumpForce));
+        }
+        jumpActive = false;
     }
     
     private void MoveHorizontal(float s) {
@@ -87,5 +99,16 @@ public class PlayerMovement : MonoBehaviour
             rigidbody.velocity = new Vector2(rigidbody.velocity.x, -maxVerticalVelocity);
         }
     }
+
+    // private bool CheckTouchingGround() 
+    // {
+    //     float touchingGroundBuffer = 1.0f;
+    //     bool onGround = false;
+    //     onGround = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0f, Vector2.down, touchingGroundBuffer, terrainLayer.value);
+    //     Debug.DrawRay(boxCollider.bounds.center, Vector2.down * boxCollider.bounds.size, Color.green);
+    //     Debug.Log(onGround);
+    //     return onGround;
+    // }
+
     #endregion
 }
