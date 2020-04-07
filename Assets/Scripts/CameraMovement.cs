c54175d01755037600ba08e2635522b9c5d780cf
Nicholas Hachmer <nicholas.hachmer@gmail.com>
2020-04-07 13:37:12 -0300

    basic camera movement that follows the players movement

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    
+    public Transform playerTransform;
+    
+    private Camera cameraComponent;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraComponent = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y, -10);
+    }
+
+
+
+}
