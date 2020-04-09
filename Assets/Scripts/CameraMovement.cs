b3627d11715421394ee682a50db469b115b94ada
Nicholas Hachmer <nicholas.hachmer@gmail.com>
2020-04-09 13:41:40 -0300

    fixed camera movement so it does not directly follow every movement of the play, instead moves to keep the player within certain bounds on the screen.

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -8,20 +8,37 @@ public class CameraMovement : MonoBehaviour
     public Transform playerTransform;
     
     private Camera cameraComponent;
-
-
-
-
-    // Start is called before the first frame update
-    void Start()
+    private short screenBufferY = 7;
+    private short screenBufferX = 10;
+    
+    void Awake()
     {
         cameraComponent = GetComponent<Camera>();
+        playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y, -10);
+
+        // Keeps player sprite within screen, but does not directly follow
+        float x = transform.position.x;
+        float y = transform.position.y;
+        float diffX = playerTransform.position.x - x;
+        float diffY = playerTransform.position.y - y;
+
+        if (diffX > screenBufferX) {
+            x = playerTransform.position.x - screenBufferX;
+        } else if (diffX < -screenBufferX) {
+            x = playerTransform.position.x + screenBufferX;
+        }
+
+        if (diffY > screenBufferY) {
+            y = playerTransform.position.y - screenBufferY;
+        } else if (diffY < -screenBufferY) {
+            y = playerTransform.position.y + screenBufferY;
+        }
+
+
+        transform.position = new Vector3(x, y, -10);
     }
 
 
