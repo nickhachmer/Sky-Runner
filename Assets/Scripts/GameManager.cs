72b9550346087c8f47f607aa9cec25307ec60859
Nicholas Hachmer <nicholas.hachmer@gmail.com>
2020-04-14 10:19:38 -0300

    created basic GameManager script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+
+    public static GameManager Instance {
+        get {
+            if (instance == null) {
+                instance = new GameManager();
+                DontDestroyOnLoad(instance);
+            }
+            return instance;
+        }
+    }
+
+    GameManager() {  }
+
+}
