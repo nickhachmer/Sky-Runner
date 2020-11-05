4ad1e11043d17e3b9f29b20b97c2463f1078de63
Nicholas Hachmer <nicholas.hachmer@gmail.com>
2020-11-04 20:14:22 -0500

    made squishy platforms

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+
+    public bool IsPlayerTouching;
+
+    void Start()
+    {
+        IsPlayerTouching = false;
+    }
+    
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag.Equals("Player"))
+        {
+            IsPlayerTouching = true;
+        }
+    } 
+    
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.tag.Equals("Player"))
+        {
+            IsPlayerTouching = false;
+        }
+    }
+
+}
