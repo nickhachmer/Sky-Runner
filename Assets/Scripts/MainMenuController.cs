1a8d65afe4e6eaff13cddec04d518e966b2fbf44
Nicholas Hachmer <nicholas.hachmer@gmail.com>
2020-11-21 13:30:22 -0500

    created the beginning of main menu

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    #region home menu
+
+    public void Play()
+    {
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    #endregion
+
+    #region options menu
+
+    public void Save()
+    {
+
+    }
+
+    public void Back()
+    {
+
+    }
+
+    #endregion
+}
